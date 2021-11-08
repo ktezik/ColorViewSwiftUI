f2c99745b9976b604e8baf5e735f69dfcb1e35b8
Ivan Grishin <ktezik@yandex.ru>
2021-11-08 15:22:06 +0300

    Initial Commit

---
+++ ColorViewSwiftUI/ColorViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorViewSwiftUIApp.swift
+//  ColorViewSwiftUI
+//
+//  Created by Иван Гришин on 08.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorViewSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorViewSwiftUI
+//
+//  Created by Иван Гришин on 08.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
